33c26e213cc981329fc10c153d387ef3a750e722
Pablo Quemé <pgqueme@gmail.com>
2018-01-14 17:45:06 -0600

    Adding node link

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const program = require('commander');
 // Require logic.js file and extract controller functions using JS destructuring assignment
 const file_creation = require('./file_creation');
