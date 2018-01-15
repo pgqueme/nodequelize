66b20e1236e5f5dee0f33e718f6e703791478d6d
Pablo Quemé <pgqueme@gmail.com>
2018-01-14 19:07:00 -0600

    First template creation

--- file_creation.js
+++ file_creation.js
@@ -1,41 +1,32 @@
 /**
- * Prints specified text to the Console.
- */
-print = (param1) => {
-    console.log(param1);
-};
-
-/**
  * Writes a file to the specified folder inside the current route.
  */
-write_file = (text, route) => {
+function write_file(text, route) {
     //File system requires
     var fs = require('fs');
     var mkdirp = require('mkdirp');
     var getDirName = require('path').dirname;
 
-    //Check if the folder exists, if not, create it
-    mkdirp(getDirName(route), function (err) {
-        if (err){
-            //Error creating folder
-            console.log(err);
-            return err;
-        }
+    return new Promise(function(resolve, reject){
+        //Check if the folder exists, if not, create it
+        mkdirp(getDirName(route), function (err) {
+            if (err){
+                //Error creating folder
+                reject(err);
+            }
             
-        //Write to the file
-        fs.writeFile(route, text, { flags: 'wx' }, function(err) {
-            if(err) {
-                //Error writing to file
-                console.log(err);
-                return err;
-            }
-            else {
-                console.log('done');
-                return true;
-            }
-        }); 
+            //Write to the file
+            fs.writeFile(route, text, { flags: 'wx' }, function(err) {
+                if(err) {
+                    //Error writing to file
+                    reject(err);
+                }
+                else {
+                    resolve('Done writing file');
+                }
+            }); 
+        });
     });
-
 }
 
-module.exports = { print, write_file };
+module.exports = { write_file };
--- index.js
+++ index.js
@@ -3,28 +3,26 @@
 const program = require('commander');
 //Methods for writing files
 const file_creation = require('./file_creation');
+const template_engine = require('./template_engine');
 
 //Version command
 program
     .version('0.0.3')
     .description('NodeJS REST API generator using Express and Sequelize');
 
-//Hello world test command
-program
-    .command('hello <param1> <param2>')
-    .alias('h')
-    .description('Hello world test')
-    .action((param1, param2) => {
-        file_creation.print(param1);
-        file_creation.print(param2);
-    });
-
+//Create files
 program
     .command('create_file <text> <route>')
     .alias('cf')
     .description('Creates a file to the same folder')
     .action((text, route) => {
-        file_creation.write_file(text, route);
+        template_engine.template_creation('templates/test', text)
+        .then(template => {
+            file_creation.write_file(template, route)
+            .then(result => {
+                console.log(result);
+            });
+        });
     });
 
 program.parse(process.argv);
